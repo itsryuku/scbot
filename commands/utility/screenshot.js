6868fc2e8a905be9e7bcc0d3268d040bc0da9345
itsryuku <39871872+itsryuku@users.noreply.github.com>
2024-03-25 13:00:27 -0400

    update screenshot.js

--- commands/utility/screenshot.js
+++ commands/utility/screenshot.js
@@ -2,6 +2,13 @@ const { SlashCommandBuilder } = require("discord.js");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 
+async function sendChunkedMessage(interaction, message) {
+  const chunks = message.match(/[\s\S]{1,2000}/g) || [];
+  for (const chunk of chunks) {
+    await interaction.followUp(chunk);
+  }
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("screenshot")
@@ -20,9 +27,14 @@ module.exports = {
       if (err) {
         return interaction.reply("File does not exist.");
       }
-
+      const startTime = new Date();
       try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({
+          headless: true,
+          ignoreHTTPSErrors: true,
+          ignoreDefaultArgs: ["--ignore-certificate-errors"],
+          timeout: 30000,
+        });
         const page = await browser.newPage();
 
         await page.setViewport({ width: 1280, height: 720 });
@@ -31,9 +43,13 @@ module.exports = {
           .readFileSync(listPath, "utf-8")
           .split("\n")
           .filter((url) => url.trim() !== "");
+
+        const statusCodes = {};
+
         for (const url of urls) {
           try {
-            await page.goto(url);
+            const response = await page.goto(url);
+            const statusCode = response.status();
             const hostname = new URL(url).hostname;
             const outputPath = `./output/${hostname}.png`;
             await page.screenshot({ path: outputPath });
@@ -41,19 +57,50 @@ module.exports = {
             const screenshotFile = fs.readFileSync(outputPath);
 
             await interaction.followUp({
-              content: `**${hostname}**`,
+              content: `\`${hostname}\` - response code: ${statusCode}`,
               files: [{ attachment: screenshotFile, name: `${hostname}.png` }],
             });
+
+            fs.unlinkSync(outputPath);
+
+            if (!statusCodes[statusCode]) {
+              statusCodes[statusCode] = [];
+            }
+            statusCodes[statusCode].push(hostname);
           } catch (error) {
             await interaction.followUp(
-              `couldn't screenshot ${url}; check console for errors.`
+              `couldn't screenshot \`${url}\`, check console for errors.`
             );
             console.error(`error while taking screenshot of ${url}:`, error);
           }
         }
 
         await browser.close();
-        interaction.followUp("done.");
+        const endTime = new Date();
+        const timeDifference = endTime - startTime;
+        const seconds = Math.floor(timeDifference / 1000);
+        const minutes = Math.floor(seconds / 60);
+
+        console.log(
+          `completed in ${seconds < 60 ? seconds : minutes} ${
+            seconds < 60 ? "second" : "minute"
+          }${seconds < 60 && seconds !== 1 ? "s" : ""}`
+        );
+
+        let report = "```\nReport:\n";
+        const topStatusCodes = Object.keys(statusCodes)
+          .sort((a, b) => statusCodes[b].length - statusCodes[a].length)
+          .slice(0, 5);
+
+        for (const code of topStatusCodes) {
+          report += `\nStatus code - ${code}:\n`;
+          statusCodes[code].forEach((website) => {
+            report += `${website}\n`;
+          });
+        }
+        report += "```";
+
+        await sendChunkedMessage(interaction, report);
       } catch (error) {
         console.error("Error:", error);
       }
--- main.js
+++ main.js
@@ -1,45 +1,109 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
+async function sendChunkedMessage(interaction, message) {
+  const chunks = message.match(/[\s\S]{1,2000}/g) || [];
+  for (const chunk of chunks) {
+    await interaction.followUp(chunk);
   }
 }
 
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith(".js"));
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("screenshot")
+    .setDescription("screenshot a list of hosts")
+    .addStringOption((option) =>
+      option
+        .setName("list")
+        .setDescription("file containing the list of hosts")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+    const listPath = interaction.options.getString("list");
 
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
+    fs.access(listPath, fs.constants.F_OK, async (err) => {
+      if (err) {
+        return interaction.reply("File does not exist.");
+      }
+      const startTime = new Date();
+      try {
+        const browser = await puppeteer.launch({
+          headless: true,
+          ignoreHTTPSErrors: true,
+          ignoreDefaultArgs: ["--ignore-certificate-errors"],
+          timeout: 30000,
+        });
+        const page = await browser.newPage();
 
-client.login(token);
+        await page.setViewport({ width: 1280, height: 720 });
+
+        const urls = fs
+          .readFileSync(listPath, "utf-8")
+          .split("\n")
+          .filter((url) => url.trim() !== "");
+
+        const statusCodes = {};
+
+        for (const url of urls) {
+          try {
+            const response = await page.goto(url);
+            const statusCode = response.status();
+            const hostname = new URL(url).hostname;
+            const outputPath = `./output/${hostname}.png`;
+            await page.screenshot({ path: outputPath });
+
+            const screenshotFile = fs.readFileSync(outputPath);
+
+            await interaction.followUp({
+              content: `\`${hostname}\` - response code: ${statusCode}`,
+              files: [{ attachment: screenshotFile, name: `${hostname}.png` }],
+            });
+
+            fs.unlinkSync(outputPath);
+
+            if (!statusCodes[statusCode]) {
+              statusCodes[statusCode] = [];
+            }
+            statusCodes[statusCode].push(hostname);
+          } catch (error) {
+            await interaction.followUp(
+              `couldn't screenshot \`${url}\`, check console for errors.`
+            );
+            console.error(`error while taking screenshot of ${url}:`, error);
+          }
+        }
+
+        await browser.close();
+        const endTime = new Date();
+        const timeDifference = endTime - startTime;
+        const seconds = Math.floor(timeDifference / 1000);
+        const minutes = Math.floor(seconds / 60);
+
+        console.log(
+          `completed in ${seconds < 60 ? seconds : minutes} ${
+            seconds < 60 ? "second" : "minute"
+          }${seconds < 60 && seconds !== 1 ? "s" : ""}`
+        );
+
+        let report = "```\nReport:\n";
+        const topStatusCodes = Object.keys(statusCodes)
+          .sort((a, b) => statusCodes[b].length - statusCodes[a].length)
+          .slice(0, 5);
+
+        for (const code of topStatusCodes) {
+          report += `\nStatus code - ${code}:\n`;
+          statusCodes[code].forEach((website) => {
+            report += `${website}\n`;
+          });
+        }
+        report += "```";
+
+        await sendChunkedMessage(interaction, report);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
+  },
+};
